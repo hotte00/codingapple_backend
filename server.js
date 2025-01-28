8d2ba2e2140ec5d85d1f853865f1387ba4330df9
hotte00 <junpio0812@kaist.ac.kr>
2025-01-29 00:52:48 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static(__dirname+'/public'))
+
+app.listen(8080, () => {
+    console.log('http://localhost:8080 에서 서버 실행중')
+})
+
+app.get('/', (요청, 응답) => {
+    응답.sendFile(__dirname+'/index.html')
+})
+
+app.get('/news', (요청, 응답) => {
+    응답.send('비올듯ㅋㅋ')
+})
+
+app.get('/shop', (요청, 응답) => {
+    응답.send('쇼핑페이지임')
+})
+
+app.get('/about', (요청, 응답) => {
+    응답.sendFile(__dirname+'/about.html')
+})
