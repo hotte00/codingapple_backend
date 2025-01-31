cc2593a02de1b567e7105c17c7795fb8ef589716
hotte00 <junpio0812@kaist.ac.kr>
2025-02-01 00:50:12 +0900

    MongoDB setup

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 const express = require('express')
 const app = express()
 
-app.use(express.static(__dirname+'/public'))
+app.use(express.static(__dirname + '/public'))
+
+const { MongoClient } = require('mongodb');
+
+let db;
+const url = 'mongodb+srv://junpio0812:gundam11@cluster0.pp3pb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+new MongoClient(url).connect().then((client) => {
+    console.log('DB연결성공')
+    db = client.db('forum');
+}).catch((err) => {
+    console.log(err)
+})
 
 app.listen(8080, () => {
     console.log('http://localhost:8080 에서 서버 실행중')
 })
 
 app.get('/', (요청, 응답) => {
-    응답.sendFile(__dirname+'/index.html')
+    응답.sendFile(__dirname + '/index.html')
 })
 
 app.get('/news', (요청, 응답) => {
@@ -20,5 +31,5 @@ app.get('/shop', (요청, 응답) => {
 })
 
 app.get('/about', (요청, 응답) => {
-    응답.sendFile(__dirname+'/about.html')
+    응답.sendFile(__dirname + '/about.html')
 })
