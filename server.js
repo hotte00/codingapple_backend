21ad44bc4a9c2c34db9d22ced8727ca22fb1bc8c
hotte00 <junpio0812@kaist.ac.kr>
2025-02-05 19:11:27 +0900

    Server Code Fix

--- server.js
+++ server.js
@@ -1,30 +1,29 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const { MongoClient } = require('mongodb');
 
 const app = express()
 
 app.use(express.static(__dirname + '/public'))
 app.set('view engine', 'ejs')
 
-mongoose.connect('mongodb://localhost:27017/forum', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then((client) => {
-    console.log('DB연결성공');
-}).catch((err) => {
-    console.log(err)
-})
-
-// const { MongoClient } = require('mongodb');
-
-// let db;
-// const url = 'mongodb://localhost:27017/forum';
-// new MongoClient(url).connect().then((client) => {
-//     console.log('DB연결성공')
-//     db = client.db('forum');
+// mongoose.connect('mongodb://localhost:27017/forum', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// }).then((client) => {
+//     console.log('DB연결성공');
 // }).catch((err) => {
 //     console.log(err)
 // })
+
+let db;
+const url = 'mongodb://127.0.0.1:27017';
+new MongoClient(url).connect().then((client) => {
+    console.log('DB연결성공')
+    db = client.db('forum');
+}).catch((err) => {
+    console.log(err)
+})
 
 app.listen(8080, () => {
     console.log('http://localhost:8080 에서 서버 실행중')
