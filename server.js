dffd80838d14b810e8dfd6b333777956319efe9f
hotte00 <junpio0812@kaist.ac.kr>
2025-02-08 00:59:48 +0900

    POST까지 수강 완료

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const app = express()
 
 app.use(express.static(__dirname + '/public'))
 app.set('view engine', 'ejs')
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 // mongoose.connect('mongodb://localhost:27017/forum', {
 //     useNewUrlParser: true,
@@ -40,7 +42,29 @@ app.get('/news', (요청, 응답) => {
 app.get('/list', async (요청, 응답) => {
     let result = await db.collection('post').find().toArray()
 
-    응답.render('list.ejs', { posts : result })
+    응답.render('list.ejs', { posts: result })
+})
+
+app.get('/write', (요청, 응답) => {
+    응답.render('write.ejs')
+})
+
+app.post('/newPost', async (요청, 응답) => {
+    console.log(요청.body)
+
+    try {
+        if (요청.body.title == '') {
+            응답.send('님 제목 입력 안하심')
+        } else {
+            await db
+                .collection('post')
+                .insertOne({ title: 요청.body.title, content: 요청.body.content })
+            응답.redirect('/')
+        }
+    } catch (e) {
+        console.log(e) ///에러 메시지 터미널에 출력
+        응답.status(500).send('서버에러남')
+    }
 })
 
 app.get('/shop', (요청, 응답) => {
